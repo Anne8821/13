447f3839a81fc7861c89c8fb69c118ba5ad8b86b
Anne8821 <makurinaa@list.ru>
2024-12-11 01:05:26 +0300

    hw12

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     String firstName;
     String lastName;
@@ -14,4 +16,23 @@ public class Author {
     public String getLastName() {
         return lastName;
     }
-}
+
+    @Override
+    public boolean eguals(Object o) {
+        if (this == o) return true;
+        if (null == o || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Object.equals(firstName, author.firstName) && Object.equals(lastName, author.lastName);
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(firstName,lastName) ; }
+
+    @Override
+    public String toString() {
+        return "Автор{" +
+                "furstname='" + firstName + '\''+
+                ", lastname='" + lastName +'\''+
+                '}';
+        }
+    }
--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,61 @@ import java.sql.SQLOutput;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Author author1 = new Author("Эрих","Ремарк");
-        System.out.println(author1.getFirstName());
+        System.out.println("level 1");
+        Author somersetMaugham = new Author("Somerset","Maugham");
+        Author somersetMaugham2 = new Author("Somerset","Maugham");
 
-        Book book = new Book("Три товарища",1932,author1);
-        System.out.println(book.getYear());
+        System.out.println(somersetMaugham2.eguals(somersetMaugham));
 
-        book.setYear(1940);
+        Book firstBook = new Book("Christmas vacation", 1939, somersetMaugham);
 
-        System.out.println(book.getYear());
+        Book thirdBook = new Book("Christmas vacation", 1939, somersetMaugham);
 
-        Author author2 = new Author("Федор","Достоевский");
-        System.out.println(author2.getLastName());
+        System.out.println(firstBook);
 
-        System.out.println(author2.getFirstName());
+        System.out.println(firstBook.getName() + " " + firstBook.getYear() + " " + firstBook.getAuthor().getFirstName() + " " + firstBook.getAuthor());
 
+        System.out.println(firstBook);
+        Author franzKafka =new Author("Franz","Kafka");
+        Book secondBook = new Book("Transformation", 1915, franzKafka);
+
+        System.out.println("----test----");
+        System.out.println(thirdBook.equals(firstBook));
+
+        System.out.println(secondBook.equals(firstBook));
+
+        System.out.println(firstBook.hashCode());
+        System.out.println(secondBook.hashCode());
+        System.out.println(thirdBook.hashCode());
+
+        firstBook.setYear(2000);
+        System.out.println(firstBook.getYear());
+
+        System.out.println("Authors:");
+        Author somersetMaugham = new Author("Somerset","Maugham");
+        Author somersetMaugham2 = new Author("Somerset","Maugham");
+        Author levTolstoy = new Author("Lev", "Tolstoy");
+        System.out.println("Author copies equal: " + somersetMaugham.eguals(somersetMaugham2));
+        System.out.println("Author copies equal by hashCode: " + (somersetMaugham.hashCode() == somersetMaugham2.hashCode()));
+        System.out.println("Author equal: " + somersetMaugham.eguals(levTolstoy));
+        System.out.println("Author equal by hashCode: " + (somersetMaugham.hashCode() == levTolstoy.hashCode()));
+
+        System.out.println(somersetMaugham);
+        System.out.println(levTolstoy);
+
+        System.out.println("---------------");
+
+        System.out.println("Books:");
+
+        Book theStand = new Book("An hour before dawn", 1942, somersetMaugham);
+
+        Book theStand2 = new Book("An hour before dawn", 1942, somersetMaugham);
+        Book warAndPeace = new Book("War and Peace", 1869, levTolstoy);
+        System.out.println("Author copies equal: " + theStand.equals(theStand2));
+        System.out.println("Author copies equal by hashCode: " + (theStand.hashCode() == theStand2.hashCode()));
+        System.out.println("Author equal: " +theStand.equals(warAndPeace));
+        System.out.println("Author equal by hashCode: " + (theStand.hashCode() == warAndPeace.hashCode()));
+        System.out.println(theStand);
+        System.out.println(warAndPeace);
     }
 }
